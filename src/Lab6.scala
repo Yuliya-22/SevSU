6423aaa6b885753911c17612e76d6fe5a6c503a2
Юлия <yuliya_taran22@mail.ru>
2025-02-15 15:34:55 +0300

    1 - 5 готовы, +6

--- src/Lab6.scala
+++ src/Lab6.scala
@@ -1,26 +1,24 @@
 
 object Lab6 {
 
+  private class Box[T] {
+    private var value: Option[T] = None
 
+    def save(v: T): Unit = { value = Some(v) }
+
+    def get: Option[T] = value
+  }
 
   def work(): Unit = {
     println("\n\n--------------------------------------------------------------Лабораторная 5\n")
-//    val calls = countSelfCalls(5)
-//    println(s"Количество собственных вызовов: $calls")
-//    println("")
-//
-//    val number = 18
-//    val base = 16
-//    val result = decimalToNBase(number, base)
-//    println(s"$base-ичное представление числа dec($number): $result")
-//    val result2 = decimalToBinary(number)
-//    println(s"Двоичное представление числа dec($number): $result2")
-//    println("")
-//
-//    val length = 4
-//    val generateList = randomListGenerator(number)
-//    val randomList = generateList(length)
-//    println(s"Случайный список чисел $number +-5 с количеством элементов $length: $randomList")
+    val intBox = new Box[Int]()
+    intBox.save(42)
+    println(intBox.get)
+    val stringBox = new Box[String]()
+    stringBox.save("Hello")
+    println(stringBox.get)
+    val emptyBox = new Box[Double]()
+    println(emptyBox.get)
+    println("")
   }
-
 }
